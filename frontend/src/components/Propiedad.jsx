5e6ceca5c463b126238ab079becc8d379b387e86
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2024-08-15 14:21:11 -0700

    Propiedad

--- frontend/src/components/Propiedad.jsx
+++ frontend/src/components/Propiedad.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Catalogo from "./Catalogo"
+import { Button, Modal } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 function Propiedad(){
+    const navigate = useNavigate();
+    const [showModalPropiedades, setShowModalPropiedades] = useState(true);
+
+    const handleClose = () => {
+        setShowModalPropiedades(false);
+        navigate(`/`);
+    }
+
     return(
         <>
             <Catalogo/>
+            <Modal show={showModalPropiedades} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Woohoo, you are reading this text in a modal!
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </>
     );
 }
