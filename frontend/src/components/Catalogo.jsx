70eda813d8ad9f06a6849e6736dc819697b7d3d4
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2024-08-15 02:31:31 -0700

    Catalogo

--- frontend/src/components/Catalogo.jsx
+++ frontend/src/components/Catalogo.jsx
@@ -1,7 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCar, faBath, faBed, faStairs, faRuler, faEye } from '@fortawesome/free-solid-svg-icons';
 import Navbar from "../components/Navbar.jsx"
 import "../css/catalogo.css"
 import { useQuery } from "react-query";
+import { Link } from 'react-router-dom';
 
 function Catalogo(){
 
@@ -41,6 +44,44 @@ function Catalogo(){
                                     <img src={item.fotoPrincipal} className="card-img-top" alt="Propiedad"></img>
                                     <div className='card-body'>
                                         <h6>{item.direccion}</h6>
+                                        <div className="card-text">
+                                            {
+                                                item.tipo === "Venta" 
+                                                ?
+                                                <>
+                                                    <div className='icons'>
+                                                        <div><i><FontAwesomeIcon icon={faCar}/></i></div>
+                                                        <div><i><FontAwesomeIcon icon={faBath}/></i></div>
+                                                        <div><i><FontAwesomeIcon icon={faBed}/></i></div>
+                                                        <div><i><FontAwesomeIcon icon={faStairs}/></i></div>
+                                                        <div><i><FontAwesomeIcon icon={faRuler}/></i></div>
+                                                    </div>
+                                                    <div className='texts'>
+                                                        <div>{item.nAutos}</div>
+                                                        <div>{item.nBanos}</div>
+                                                        <div>{item.nRecamaras}</div>
+                                                        <div>{item.nEscaleras}</div>
+                                                        <div>{item.metros}</div>
+                                                    </div>
+                                                </>
+                                                    
+                                                :
+
+                                                <>
+                                                    <div className='icon'>
+                                                        <div><i><FontAwesomeIcon icon={faRuler}/></i></div>
+                                                    </div>
+                                                    <div className='text'>
+                                                        <div>{item.metros}</div>
+                                                    </div>
+                                                </>
+                                            }
+                                            <div className='tipo'>{item.tipo.toUpperCase()}</div>
+                                            <div className='precio'>${item.precio.$numberDecimal}</div>
+                                        </div>
+                                    </div>
+                                    <div className='card-footer'>
+                                        <a style={{cursor: "pointer"}}><i><FontAwesomeIcon icon={faEye}/></i> VER</a>
                                     </div>
                                 </div>
                             ))}
