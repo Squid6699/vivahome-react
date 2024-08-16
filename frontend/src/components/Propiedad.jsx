9087755cfb3b8d06f34814924370c67c5e23f555
Squid6699 <brayanaviles96@gmail.com>
2024-08-15 21:33:54 -0700

    carrusel

--- frontend/src/components/Propiedad.jsx
+++ frontend/src/components/Propiedad.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import "../css/propiedad.css";
 import Catalogo from "./Catalogo"
-import { Button, Carousel, Modal, Placeholder } from 'react-bootstrap';
+import { Button, Card, Carousel, Modal, Placeholder } from 'react-bootstrap';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useQuery } from "react-query";
 
@@ -51,6 +51,11 @@ function Propiedad(){
                                 <Modal.Title>CARGANDO...</Modal.Title>
                             </Modal.Header>
                             <Modal.Body>
+                                <Carousel activeIndex={index} onSelect={handleSelect}>
+                                    <Carousel.Item>
+                                        <Card.Img variant="top" src="holder.js/100px180" />
+                                    </Carousel.Item>
+                                </Carousel>
                                 <Placeholder xs={6} />
                             </Modal.Body>
                             <Modal.Footer>
@@ -68,19 +73,28 @@ function Propiedad(){
                         {
                             propiedad ?
                             <>
-                                <Modal show={showModalPropiedades} onHide={handleClose}>
+                                <Modal show={showModalPropiedades} onHide={handleClose} size='lg' dialogClassName='modal-dialog-scrollable'>
                                     <Modal.Header closeButton>
-                                        <Modal.Title className='fs-5'>{propiedad.direccion}</Modal.Title>
+                                        <Modal.Title><h5>{propiedad.direccion}</h5></Modal.Title>
                                     </Modal.Header>
                                     <Modal.Body>
                                         <Carousel activeIndex={index} onSelect={handleSelect}>
-                                            <Carousel.Item>
-                                                {/* <img src={propiedad.Habitaciones[1].img1} alt="" /> */}
-                                                <Carousel.Caption>
-                                                <h3>First slide label</h3>
-                                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                                                </Carousel.Caption>
-                                            </Carousel.Item>
+                                            {
+                                                propiedad.fotos.map((item) => (
+                                                    <Carousel.Item>
+                                                        <img src={item.img1} alt="" />
+                                                    </Carousel.Item>
+                                                    
+                                                ))
+                                            }
+                                            {
+                                                propiedad.fotos.map((item) => (
+                                                    <Carousel.Item>
+                                                        <img src={item.img2} alt="" />
+                                                    </Carousel.Item>
+                                                    
+                                                ))
+                                            }
                                         </Carousel>
                                         <div className='container'>{propiedad.descripcion}</div>
                                     </Modal.Body>
