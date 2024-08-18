4d4ade6ba28d81278f097edcce1297b69224d71f
Squid6699 <brayanaviles96@gmail.com>
2024-08-17 23:40:29 -0700

    Home

--- frontend/src/components/Inicio.jsx
+++ frontend/src/components/Inicio.jsx
@@ -4,14 +4,12 @@ import "../css/inicio.css"
 import Navbar from "./Navbar.jsx"
 import Filtros from './Filtros.jsx';
 import Catalogo from './Catalogo.jsx';
-import Botonera from './Botonera.jsx';
 
 function Inicio(){
     return(
         <>
             <Navbar />
-            <Botonera/>
-            <section className='containerApp'>
+            <section className='containerApp' id = "containerApp">
                 <Filtros/>
                 <Catalogo/>
             </section>
