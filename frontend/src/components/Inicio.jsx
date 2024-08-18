d9aa10b40234776c2312fb0160c14ca7df959ee4
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2024-08-18 15:03:51 -0700

    filtros

--- frontend/src/components/Inicio.jsx
+++ frontend/src/components/Inicio.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../css/inicio.css"
 
 import Navbar from "./Navbar.jsx"
 import Filtros from './Filtros.jsx';
 import Catalogo from './Catalogo.jsx';
+import { Button } from 'react-bootstrap';
 
 function Inicio(){
+    const [mostrarFiltros, setMostrarFiltros] = useState(window.localStorage.getItem("filtros") === "true" || false);
+
+    const toggleFiltros = () => {
+        setMostrarFiltros(!mostrarFiltros);
+        window.localStorage.setItem("filtros", !mostrarFiltros);
+    };
+
     return(
         <>
             <Navbar />
-            <section className='containerApp'>
-                <Filtros/>
+            <section className={`containerApp ${!mostrarFiltros ? 'ocultar-filtros' : ''}`}>
+                <section className='botones'>
+                    <Button className='botonesStyle' onClick={toggleFiltros}>
+                        {mostrarFiltros ? 'OCULTAR FILTROS' : 'MOSTRAR FILTROS'}
+                    </Button>
+                </section>
+                {mostrarFiltros && <Filtros/>}
                 <Catalogo/>
             </section>
             
