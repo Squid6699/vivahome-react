5cab92ee08d6f2f02579bd4675dd5fe86a73f0c6
Squid6699 <brayanaviles96@gmail.com>
2024-08-17 23:05:35 -0700

    Add Botonera

---
+++ frontend/src/components/Botonera.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Botonera(){
+    return(
+        <>
+            <section className='tags'>
+                <div>AQUI VAN LOS BOTONES DE FILTROS Y MAPA</div>
+            </section>
+        </>
+    );
+}
+
+export default Botonera;
--- frontend/src/components/Inicio.jsx
+++ frontend/src/components/Inicio.jsx
@@ -4,12 +4,13 @@ import "../css/inicio.css"
 import Navbar from "./Navbar.jsx"
 import Filtros from './Filtros.jsx';
 import Catalogo from './Catalogo.jsx';
-
+import Botonera from './Botonera.jsx';
 
 function Inicio(){
     return(
         <>
             <Navbar />
+            <Botonera/>
             <section className='containerApp'>
                 <Filtros/>
                 <Catalogo/>
