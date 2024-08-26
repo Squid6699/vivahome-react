d8a6be6e0d359f2a154232e32786d5ccc8781964
Squid6699 <brayanaviles96@gmail.com>
2024-08-25 23:00:49 -0700

    verPropiedadePublicadas backend

--- backend/routes/verPropiedadesPublicadas.js
+++ backend/routes/verPropiedadesPublicadas.js
@@ -15,15 +15,20 @@ routePropiedadesPublicadas.post("/", async (req, res) => {
         const idUsuario = await Usuario.findOne({correo: correo});
 
         if (!idUsuario) {
-            return res.json({success: false, error: "OCURRIO UN ERROR AL OBTENER DATOS DEL USUARIO"})
+            return res.json({success: false, error: "OCURRIO UN ERROR AL OBTENER DATOS DEL USUARIO"});
         }
 
         const propiedades = await Propiedades.find({idUsuario: idUsuario._id});
 
-        return res.json({success: true, propiedades: propiedades});
+        if (propiedades.length > 0){
+            return res.json({success: true, propiedades: propiedades});
+        }else{
+            return res.json({success: false, error: "SIN PROPIEDADES PUBLICADAS"});
+        }
+
 
     } catch (error) {
-        
+        return res.json({success: false, error: "OCURRIO UN ERROR AL INTENTAR RESOLVER SU SOLICITUD"});
     }
     
 })
