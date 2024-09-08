7715fa16e554042c4dc96e90ef63cbdaafd488d2
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2024-09-07 17:51:34 -0700

    verMisPropiedades Modal

--- frontend/src/components/Catalogo.jsx
+++ frontend/src/components/Catalogo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import "../css/catalogo.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCar, faBath, faBed, faStairs, faRuler, faEye } from '@fortawesome/free-solid-svg-icons';
--- frontend/src/components/VerPropiedadesPublicadas.jsx
+++ frontend/src/components/VerPropiedadesPublicadas.jsx
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react'
 import { Modal } from 'react-bootstrap';
 import { HOST } from '../../config';
 import { useQuery } from 'react-query';
+import "../css/VerPropiedadesPublicadas.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBath, faBed, faCar, faEye, faRuler, faStairs } from '@fortawesome/free-solid-svg-icons';
 
 function VerPropiedadesPublicadas({showPropiedades, handleClosePropiedades}){
     const { data: misPropiedades, isLoading, refetch, isRefetching } = useQuery("MisPropiedades", obtenerMisPropiedades);
@@ -30,12 +33,68 @@ function VerPropiedadesPublicadas({showPropiedades, handleClosePropiedades}){
 
     return(
         <>
-            <Modal show={showPropiedades} size="xl" onHide={handleClosePropiedades} backdrop="static" keyboard={false}>
+            <Modal show={showPropiedades} fullscreen = {true} scrollable = {true} onHide={handleClosePropiedades} backdrop="static" keyboard={false} >
                 <Modal.Header closeButton>
                     <Modal.Title>MIS PROPIEDADES</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>
-
+                <Modal.Body className='containerApp-misPropiedades'>
+                    <section className='container-misPropiedades'>
+                        {isLoading || isRefetching ? (
+                            <span className='textCatalogo-misPropiedades'>CARGANDO PROPIEDADES...</span>
+                        ) : (
+                        <>
+                            {misPropiedades && misPropiedades.length > 0 ? 
+                                <div className='catalogo-misPropiedades'>
+                                    {misPropiedades.map((item) => (
+                                        <div key={item._id} className='card'>
+                                            <img src={item.fotoPrincipal} className="card-img-top" alt="Propiedad"></img>
+                                            <div className='card-body'>
+                                                <h6>{item.direccion}</h6>
+                                                <div className="card-text">
+                                                    {
+                                                        item.tipo !== "Terreno" 
+                                                        ?
+                                                        <>
+                                                            <div className='icons'>
+                                                                <div><i><FontAwesomeIcon icon={faCar}/></i></div>
+                                                                <div><i><FontAwesomeIcon icon={faBath}/></i></div>
+                                                                <div><i><FontAwesomeIcon icon={faBed}/></i></div>
+                                                                <div><i><FontAwesomeIcon icon={faStairs}/></i></div>
+                                                                <div><i><FontAwesomeIcon icon={faRuler}/></i></div>
+                                                                <div>{item.nAutos}</div>
+                                                                <div>{item.nBanos}</div>
+                                                                <div>{item.nRecamaras}</div>
+                                                                <div>{item.nEscaleras}</div>
+                                                                <div>{item.metros}<small>m²</small></div>
+                                                            </div>
+                                                        </>
+                                                            
+                                                        :
+                                                        <>
+                                                            <div className='icon'>
+                                                                <div><i><FontAwesomeIcon icon={faRuler}/></i></div>
+                                                            </div>
+                                                            <div className='text'>
+                                                                <div>{item.metros}<small>m²</small></div>
+                                                            </div>
+                                                        </>
+                                                    }
+                                                    <div className='tipo'>{item.tipo.toUpperCase()}</div>
+                                                    <div className='precio'>${item.precio.$numberDecimal}</div>
+                                                </div>
+                                            </div>
+                                            <div className='card-footer'>
+                                                <span><i><FontAwesomeIcon icon={faEye}/></i> VER</span>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                                : 
+                                <span className='textCatalogo-misPropiedades'>NO TIENES PROPIEDADES PUBLICADAS.</span>
+                            }
+                        </>
+                        )}
+                    </section>
                 </Modal.Body>
             </Modal>
         </>
