d8e6c06f92d4e0cb9e933d7790877375a82be248
Lil Squid <80144515+Squid6699@users.noreply.github.com>
2024-09-08 16:16:19 -0700

    VerPropiedadesPublicadas

--- frontend/src/components/Propiedad.jsx
+++ frontend/src/components/Propiedad.jsx
@@ -65,16 +65,16 @@ function Propiedad(){
                                 <Modal.Title>CARGANDO...</Modal.Title>
                             </Modal.Header>
                             <Modal.Body>
-                            <h5 className="card-title placeholder-glow">
-                                <span className="placeholder col-6"></span>
-                            </h5>
-                            <p className="card-text placeholder-glow">
-                                <span className="placeholder col-7"></span>
-                                <span className="placeholder col-4"></span>
-                                <span className="placeholder col-4"></span>
-                                <span className="placeholder col-6"></span>
-                                <span className="placeholder col-8"></span>
-                            </p>
+                                <h5 className="card-title placeholder-glow">
+                                    <span className="placeholder col-6"></span>
+                                </h5>
+                                <p className="card-text placeholder-glow">
+                                    <span className="placeholder col-7"></span>
+                                    <span className="placeholder col-4"></span>
+                                    <span className="placeholder col-4"></span>
+                                    <span className="placeholder col-6"></span>
+                                    <span className="placeholder col-8"></span>
+                                </p>
                             </Modal.Body>
                             <Modal.Footer>
                                 <Button variant="secondary" onClick={handleClose}>
--- frontend/src/components/VerPropiedadesPublicadasVer.jsx
+++ frontend/src/components/VerPropiedadesPublicadasVer.jsx
@@ -1,23 +1,92 @@
-import React from 'react'
-import { Button, Modal } from 'react-bootstrap';
+import { faBath, faBed, faCar, faRuler, faStairs } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react'
+import { Carousel, Modal } from 'react-bootstrap';
+import "../css/verPropiedadesPublicadasVer.css"
 
 function VerPropiedadesPublicadasVer({showVerPropiedadesVer, handleCloseVerPropiedades, propiedad}){
+    const [indexCarrusel, setIndexCarrusel] = useState(0);
+
+    const handleSelect = (selectedIndex) => {
+        setIndexCarrusel(selectedIndex);
+    };
+
+    console.log(propiedad);
     return(
         <>
-            <Modal show={showVerPropiedadesVer} onHide={handleCloseVerPropiedades}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+            <Modal show={showVerPropiedadesVer} onHide={handleCloseVerPropiedades} size='xl' backdrop= "static">
+                <Modal.Header style={{border: "none"}} closeButton></Modal.Header>
+                <Modal.Body>
+                    <div className="card mb-3" style={{maxWidth: "1000x"}}>
+                        <div className="row g-0">
+                            <div className="col-md-4">
+                                <Carousel activeIndex={indexCarrusel} onSelect={handleSelect}>
+                                    {
+                                        propiedad.fotos.map((item, index) => (
+                                            <Carousel.Item key={index}>
+                                                <img src={item.img1} alt="" />
+                                            </Carousel.Item>
+                                            
+                                        ))
+                                    }
+                                    {
+                                        propiedad.fotos.map((item, index) => (
+                                            <Carousel.Item key={index}>
+                                                <img src={item.img2} alt="" />
+                                            </Carousel.Item>
+                                            
+                                        ))
+                                    }
+                                </Carousel>
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h5 className="card-title" style={{marginBottom: "20px", textAlign: "center"}}>{propiedad.direccion}</h5>
+                                    <div className="card-text">
+                                        {
+                                            propiedad.tipo !== "Terreno" 
+                                            ?
+                                            <>
+                                                <div className='icons'>
+                                                    <div><i><FontAwesomeIcon icon={faCar}/></i></div>
+                                                    <div><i><FontAwesomeIcon icon={faBath}/></i></div>
+                                                    <div><i><FontAwesomeIcon icon={faBed}/></i></div>
+                                                    <div><i><FontAwesomeIcon icon={faStairs}/></i></div>
+                                                    <div><i><FontAwesomeIcon icon={faRuler}/></i></div>
+                                                    <div>{propiedad.nAutos}</div>
+                                                    <div>{propiedad.nBanos}</div>
+                                                    <div>{propiedad.nRecamaras}</div>
+                                                    <div>{propiedad.nEscaleras}</div>
+                                                    <div>{propiedad.metros}<small>m²</small></div>
+                                                </div>
+                                            </>
+                                                
+                                            :
+                                            <>
+                                                <div className='icon'>
+                                                    <div><i><FontAwesomeIcon icon={faRuler}/></i></div>
+                                                </div>
+                                                <div className='text'>
+                                                    <div>{propiedad.metros}<small>m²</small></div>
+                                                </div>
+                                            </>
+                                        }
+                                        <div className='tipo'>{propiedad.tipo.toUpperCase()}</div>
+                                        <div className='precio'>${propiedad.precio.$numberDecimal}</div>
+                                        <div className='descripcion'>{propiedad.descripcion}</div>
 
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleCloseVerPropiedades}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={handleCloseVerPropiedades}>
-                        Save Changes
-                    </Button>
-                </Modal.Footer>
+                                        <div className='estado'>
+                                            <span>ESTADO <br /></span>
+                                            {!propiedad.autorizada && <span className='autorizado'>NO AUTORIZADA</span>}
+                                            {propiedad.disponible === "DISPONIBLE" && <span className='disponible'>DISPONIBLE</span>}
+                                            {propiedad.disponible === "VENDIDA" && <span className='vendida'>VENDIDA</span>}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Modal.Body>
             </Modal>
         </>
     );
